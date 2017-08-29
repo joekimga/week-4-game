aef761e623502a687c3eeeda0099ca4c21e9b0b6
Joseph Kim <joekimga@gmail.com>
2017-08-29 19:05:56 -0400

    WIP

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+alert("Are you ready to play?");
+
+
+/*
+var targetNumber 112;
+
+$("#number-to-guess").text(targetNumber);
+
+var counter = 0;
+
+//array
+var numberOptions = [10, 5, 3, 1];
+
+//for loop
+for (var i = 0; i < numberOptions.length; i++) {
+	numberOptions[i]
+console.log("numberOptions");
+
+}
+*/
+/*
+for (var i = 0; i < 4; i++) {
+
+	var random = Math.floor(Math.random() * 12);
+	console.log(random);
+
+	var crystal = $("<div>");
+		crystal.attr({
+			"class": 'crystal'
+			"data-random": random
+
+			});
+		$(".crystals").append(crystal);
+}
+
+*/
